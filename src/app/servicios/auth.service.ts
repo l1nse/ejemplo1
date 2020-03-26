fd4ba60f11a5279e6b378f191f7d26f0eadcf1b8
Gustavo <gustavoignacio01@gmail.com>
2020-03-26 16:51:56 -0300

    Boton logout terminado

--- src/app/servicios/auth.service.ts
+++ src/app/servicios/auth.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth'
 import { resolve } from 'url';
+import { Router } from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private AFauth : AngularFireAuth) {  }
+  constructor(private AFauth : AngularFireAuth,
+              private router : Router ) { }
+
+  registerEmail(email : string , password :string)
+  {
+    return new Promise((resolve , rejected) => {
+      this.AFauth.auth.createUserWithEmailAndPassword(email , password).then(res =>{
+        resolve(res)
+      }).catch(err => rejected(err));   
+    } );   
+  }
 
   loginEmail( email : string , password : string)
   {
@@ -21,12 +32,11 @@ export class AuthService {
     } );    
   }
 
-  registerEmail(email : string , password :string)
-  {
-    return new Promise((resolve , rejected) => {
-      this.AFauth.auth.createUserWithEmailAndPassword(email , password).then(res =>{
-        resolve(res)
-      }).catch(err => rejected(err));   
-    } );   
+  logOut( )
+  { 
+       this.AFauth.auth.signOut().then(auth => {
+          this.router.navigate(['/login']);
+      })    
   }
+
 }
